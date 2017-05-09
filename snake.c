447eb8989c4cbbf75ca29ba0b07fb0a167a6d9b1
dshedd <dan@dshedd.com>
2017-05-08 21:04:11 -0400

    Added score, game over, and arrow key mappings

--- snake.c
+++ snake.c
@@ -25,7 +25,8 @@
 		maxX = 0,
 		nextX = 0,
 		nextY = 0,
-		tailLength = 5;
+		tailLength = 5,
+		score = 0;
 
 	bool gameOver = false;
 
@@ -49,9 +50,13 @@
 	void cursesInit() {
 		initscr(); //Initialize the window
 		noecho(); //Don't echo keypresses
+		keypad(stdscr, TRUE);
 		cbreak();
 		timeout(TIMEOUT);
 		curs_set(FALSE); //Don't display a cursor
+
+		//Global var stdscr is created by the call to initscr()
+		getmaxyx(stdscr, maxY, maxX);
 	}
 
 	void init() {
@@ -60,6 +65,7 @@
 		currentDir = RIGHT;
 		tailLength = 5;
 		gameOver = false;
+		score = 0;
 
 		clear(); //Clears the screen
 		
@@ -69,14 +75,12 @@
 			point currPoint;
 			
 			currPoint.x = i;
-			currPoint.y = 0;
+			currPoint.y = maxY / 2; //Start mid screen on the y axis
 
 			snakeParts[j] = currPoint;
 			j++;
 		}
 
-		//Global var stdscr is created by the call to initscr()
-		getmaxyx(stdscr, maxY, maxX);
 
 		createFood();
 
@@ -112,13 +116,13 @@
 			/* Input Handler */
 				ch = getch();
 
-				if(( ch=='l' || ch=='L' ) && (currentDir != RIGHT && currentDir != LEFT)) {
+				if(( ch=='l' || ch=='L' || ch == KEY_RIGHT) && (currentDir != RIGHT && currentDir != LEFT)) {
 					currentDir = RIGHT;
-				} else if (( ch=='h' || ch=='H' ) && (currentDir != RIGHT && currentDir != LEFT)) {
+				} else if (( ch=='h' || ch=='H' || ch == KEY_LEFT) && (currentDir != RIGHT && currentDir != LEFT)) {
 					currentDir = LEFT;
-				} else if((ch=='j' || ch=='J') && (currentDir != UP && currentDir != DOWN)) {
+				} else if((ch=='j' || ch=='J' || ch == KEY_DOWN) && (currentDir != UP && currentDir != DOWN)) {
 					currentDir = DOWN;
-				} else if((ch=='k' || ch=='K') && (currentDir != UP && currentDir != DOWN)) {
+				} else if((ch=='k' || ch=='K' || ch == KEY_UP) && (currentDir != UP && currentDir != DOWN)) {
 					currentDir = UP;
 				}
 
@@ -138,14 +142,27 @@
 
 					snakeParts[tailLength] = tail;
 
-					tailLength++;
+					if(tailLength < 255)
+						tailLength++;
+					else
+						tailLength = 5; //If we have exhausted the array then just reset the tail length but let the player keep building their score :)
 
+					score += 5;
 					createFood();
 				} else {
+					//Draw the snake to the screen
+					for(int i = 0; i < tailLength; i++) {
+						if(nextX == snakeParts[i].x && nextY == snakeParts[i].y) {
+							gameOver = true;
+							break;
+						}
+					}
+
 					//We are going to set the tail as the new head
 					snakeParts[tailLength - 1].x = nextX;
 					snakeParts[tailLength - 1].y = nextY;
 				}
+
 				//Shift all the snake parts
 				shiftSnake();
 
@@ -154,9 +171,12 @@
 
 			//Game Over if the player hits the screen edges
 			if((nextX >= maxX || nextX < 0) || (nextY >= maxY || nextY < 0)) {
-				mvprintw(maxY / 2, maxX / 2, "Game Over!");
 				gameOver = true;
 			}
+
+			//Print game over if gameOver is true
+			if(gameOver)
+				mvprintw(maxY / 2, maxX / 2, "Game Over!");
 
 			//Draw the snake to the screen
 			for(int i = 0; i < tailLength; i++) {
@@ -165,6 +185,9 @@
 
 			//Draw the current food
 			drawPart(food);
+
+			//Draw the score
+			mvprintw(1, 2, "Score: %i", score);
 
 			//ncurses refresh
 			refresh();
